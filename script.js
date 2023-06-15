a9e751556408fb01a57a6bf980a9bcf485820629
Yogesh Sankhala <133505791+yogeshsankhala@users.noreply.github.com>
2023-06-15 13:25:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+/*----------sticky-header-----------*/
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1) {
+        $('.site-header').addClass("sticky-header");
+    } else {
+        $('.site-header').removeClass("sticky-header");
+    }
+});
+
+/*----------Menu Toggle----------*/
+$(".menu-toggle").click(function() {
+    $(this).toggleClass("on");
+    $(".navbar").slideToggle();
+});
+
+/*-----------Responsive-Tab-------------*/
+$(document).ready(function() {
+    $('#responsiveTabsDemo').responsiveTabs({
+        startCollapsed: 'accordion'
+    });
+});
+
+/*-------Scroll to top----------*/
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.top').fadeIn();
+    } else {
+        $('.top').fadeOut();
+    }
+});
+
+$('.top').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+/*----------- Accordian-------------*/
+$(".about__vision h2").click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('active');
+});
+
+$(".accordian__block h6").click(function() {
+    if ($(this).hasClass("active")) {
+        $(this).removeClass('active')
+        $(this).next().slideUp();
+    } else {
+        $('.accordian__content').slideUp();
+        $('.accordian__block h6').removeClass('active');
+        $(this).siblings('.accordian__content').slideToggle();
+        $(this).toggleClass('active');
+    }
+    return false;
+});
+
+$(function() {
+    var title = $(".checkout_accordian-icon");
+    title
+        .on("click", function() {
+            var ThisIt = $(this);
+            $(".checkout_accordian-icon").not(ThisIt).removeClass("checkout-active");
+            $(this).toggleClass("checkout-active");
+            $(".checkout_accordian").not(ThisIt.next(".checkout_accordian")).slideUp(400);
+            ThisIt.next(".checkout_accordian").slideToggle(400);
+        })
+        .eq(0)
+        .click();
+});
+
+/*-------Number-count--------*/
+$('.count').each(function() {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function(now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
